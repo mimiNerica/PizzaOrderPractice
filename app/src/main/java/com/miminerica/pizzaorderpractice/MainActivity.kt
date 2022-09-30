0681b3ce5a55b46b460d6d914e6ab9834563f927
mimi <kuni2097@gmail.com>
2022-09-30 14:47:31 +0900

    BaseActivity 작성

--- app/src/main/java/com/miminerica/pizzaorderpractice/MainActivity.kt
+++ app/src/main/java/com/miminerica/pizzaorderpractice/MainActivity.kt
@@ -3,9 +3,20 @@ package com.miminerica.pizzaorderpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
 }
